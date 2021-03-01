d288f19784ad0b8734674bf67a624ce3add4b858
Felix Pantoja <fpantoja26@icloud.com>
2021-03-01 17:29:52 -0400

    left-right nav

---
+++ Navbarleft-right/app.js
@@ -0,0 +1,6 @@
+let navBar = document.getElementById("nav-l");
+let navTog = document.getElementById("nav-t");
+
+navTog.addEventListener("click", function () {
+  navBar.classList.toggle("active");
+});
